5ff01390b79478834b9ba601d0d7a783c292cc5c
aavishkarmishra <aavishkarmishra@gmail.com>
2022-04-26 15:47:18 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function submit() {
+  var inputFormat = parseInt(document.getElementById('inputFormat').value);
+  var outputFormat = parseInt(document.getElementById('outputFormat').value);
+  var input = document.getElementById('input').value;
+  var ouptut;
+  if (inputFormat == outputFormat) {
+    ouptut = input;
+  } else {
+    ouptut = format(input, inputFormat, outputFormat);
+  }
+
+  document.getElementById('output').value = ouptut;
+}
+
+function format(input, inputFormat, outputFormat) {
+  var temp, output;
+  switch (inputFormat) {
+    case 1:
+      temp = parseInt(input, 2);
+      break;
+    case 2:
+      temp = parseInt(input, 8);
+      break;
+    case 3:
+      temp = parseInt(input, 10);
+      break;
+    case 4:
+      temp = parseInt(input, 16);
+      break;
+  }
+  switch (outputFormat) {
+    case 1:
+      output = temp.toString(2);
+      break;
+    case 2:
+      output = temp.toString(8);
+      break;
+    case 3:
+      output = temp.toString(10);
+      break;
+    case 4:
+      output = temp.toString(16);
+      break;
+  }
+  return output;
+}
